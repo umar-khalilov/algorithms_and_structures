efcbfb66bbe0e7702c2b68d58d3a3149f89546ef
Umar Khalilov <ERMASTER100@gmail.com>
2023-02-28 14:30:33 +0200

    Add linear search decision

---
+++ src/1-linearSearch.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const arrOfNumbers = [1, 4, 5, 8, 6, 5, 1, 2, 7, 5, 2, 12];
+
+const linearSearch = (array = [], item) => {
+    const length = array.length;
+    for (let index = 0; index < length; index++) {
+        console.count('count');
+        if (array[index] === item) return index;
+    }
+    return null;
+};
+
+console.log(linearSearch(arrOfNumbers, 1));
